56b8e8989f6575ceb36d9442a0b585cac857d659
tamisan <phongphat.jap@gmail.com>
2024-11-09 09:57:36 +0700

    EP42

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,5 +1,5 @@
 // rafce
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useForm } from "react-hook-form";
@@ -20,6 +20,8 @@ const registerSchema = z
 
 const Register = () => {
   // Javascript
+  const [passwordScore, setPasswordScore] = useState(0);
+
   const {
     register,
     handleSubmit,
@@ -29,22 +31,22 @@ const Register = () => {
     resolver: zodResolver(registerSchema),
   });
 
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-
+  const validatePassword = () => {
+    let password = watch().password;
+    return zxcvbn(password ? password : "").score;
+  };
+  useEffect(() => {
+    setPasswordScore(validatePassword());
+  }, [watch().password]);
 
   const onSubmit = async (data) => {
-    const passwordScore = zxcvbn(data.password).score
-    console.log(passwordScore)
-    if(passwordScore<3){
-      toast.warning('Password บ่ Strong!!!!!')
-      return
-    }
-    console.log('ok ลูกพี่')
+    // const passwordScore = zxcvbn(data.password).score;
+    // console.log(passwordScore);
+    // if (passwordScore < 3) {
+    //   toast.warning("Password บ่ Strong!!!!!");
+    //   return;
+    // }
+    // console.log("ok ลูกพี่");
     // Send to Back
     try {
       const res = await axios.post("http://localhost:5001/api/register", data);
@@ -58,25 +60,100 @@ const Register = () => {
     }
   };
 
+  // const tam = Array.from(Array(5))
+  // console.log(tam)
+  console.log(passwordScore);
   return (
-    <div>
-      Register
-      <form onSubmit={handleSubmit(onSubmit)}>
-        Email
-        <input {...register("email")} className="border" />
-        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
-        Password
-        <input {...register("password")} className="border" />
-        {errors.password && (
-          <p className="text-red-500">{errors.password.message}</p>
-        )}
-        Confirm Password
-        <input {...register("confirmPassword")} className="border" />
-        {errors.confirmPassword && (
-          <p className="text-red-500">{errors.confirmPassword.message}</p>
-        )}
-        <button className="bg-blue-500 rounded-md">Register</button>
-      </form>
+    <div
+      className="min-h-screen flex 
+    items-center justify-center bg-gray-100"
+    >
+      <div className="w-full shadow-md bg-white p-8 max-w-md">
+        <h1 className="text-2xl text-center my-4 font-bold">Register</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-4">
+            <div>
+              <input
+                {...register("email")}
+                placeholder="Email"
+                className={`border w-full px-3 py-2 rounded
+            focus:outline-none focus:ring-2 focus:ring-blue-500
+            focus:border-transparent
+            ${errors.email && "border-red-500"}
+            `}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm">{errors.email.message}</p>
+              )}
+            </div>
+
+            <div>
+              <input
+                {...register("password")}
+                placeholder="Password"
+                type="password"
+                className={`border w-full px-3 py-2 rounded
+              focus:outline-none focus:ring-2 focus:ring-blue-500
+              focus:border-transparent
+              ${errors.password && "border-red-500"}
+              `}
+              />
+
+              {errors.password && (
+                <p className="text-red-500 text-sm">
+                  {errors.password.message}
+                </p>
+              )}
+              {watch().password?.length > 0 && (
+                <div className="flex mt-2">
+                  {Array.from(Array(5).keys()).map((item, index) => (
+                    <span className="w-1/5 px-1" key={index}>
+                      <div
+                        className={`rounded h-2 ${
+                          passwordScore <= 2
+                            ? "bg-red-500"
+                            : passwordScore < 4
+                            ? "bg-yellow-500"
+                            : "bg-green-500"
+                        }
+              `}
+                      ></div>
+                    </span>
+                  ))}
+                </div>
+              )}
+            </div>
+
+            <div>
+              <input {...register("confirmPassword")} 
+              type="password"
+               placeholder="Confirm Password"
+              className={`border w-full px-3 py-2 rounded
+                focus:outline-none focus:ring-2 focus:ring-blue-500
+                focus:border-transparent
+                ${errors.confirmPassword && "border-red-500"}
+                `}
+                />
+
+
+              {errors.confirmPassword && (
+                <p className="text-red-500 text-sm">{errors.confirmPassword.message}</p>
+              )}
+            </div>
+
+            <button 
+            className="bg-blue-500 rounded-md
+             w-full text-white font-bold py-2 shadow
+             hover:bg-blue-700
+             ">
+              Register
+              </button>
+
+
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
